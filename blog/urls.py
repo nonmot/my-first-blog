40d98d26d11770a4d73e472fb90cb8b80b9da44c
mot <nonmot0614@gmail.com>
2020-11-19 16:03:52 +0900

    create postform and detail

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('post', views.post_new, name='post'),
+    path('detail/<int:pk>', views.post_detail, name='detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Post
+from .forms import PostForm
+from django.utils import timezone
+from django.shortcuts import redirect
 
 # Create your views here.
 def home(request):
@@ -8,3 +11,27 @@ def home(request):
         'posts': posts
     }
     return render(request, 'blog/post.html', params)
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('/')
+
+    else:
+        form = PostForm()
+    params = {
+        'form': form,
+    }
+    return render(request, 'blog/post_create.html', params)
+
+def post_detail(request, pk):
+    post = Post.objects.get(id=pk)
+    params = {
+        'post': post
+    }
+    return render(request, 'blog/post_detail.html', params)
